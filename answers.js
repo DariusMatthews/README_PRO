8acaa7289f43cda874eea5796cebc66bb30fc533
Darius Matthews <DariustheDeveloper@gmail.com>
2020-08-31 14:13:00 -0500

    Fixed Directory bug

--- answers.js
+++ answers.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 // Generating README
-const readMe = ({ title, desc, install, usage, contribution, license, github, email }) => {
+const readMe = ({ title, desc, install, usage, contributing, license, github, email }) => {
   // get different shield based on license
-  let sheild;
+  let sheild = '';
   if (license === 'GPLv3') {
     shield = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
   } else if (license === 'MIT') {
@@ -12,9 +12,11 @@ const readMe = ({ title, desc, install, usage, contribution, license, github, em
   } else {
     shield = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
   }
+  console.log(license, sheild);
 
   // README content
-  const content = `# ${title} ${sheild}
+  const content = `# ${title}
+  ${sheild}
 
   ## Description
 
@@ -38,20 +40,20 @@ const readMe = ({ title, desc, install, usage, contribution, license, github, em
 
   ## Contributors
 
-  ${contribution}
+  ${contributing}
 
   ## Questions
 
   Follow my [github](https://github.com/${github}).
 
-  Email me [here](mailto:${email}?subject=[github%20-${title}]).
+  Email me [here](mailto:${email}?subject=[github-${github}]).
 
   ## License
 
   Licensed under the ${license} license.`
 
   // make Created Directory
-  fs.mkdir(path.join(__dirname, 'created'), err => { if (err) throw err });
+  fs.mkdirSync(path.join(__dirname, 'created'), err => { if (err) throw err });
 
   // Create README.md file in Created folder
   fs.writeFile(path.join(`${__dirname}/created`, 'README.md'), content, err => {
